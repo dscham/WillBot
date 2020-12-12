aff7b1c0c06391073a811b5ce635431a4aaa3e0a
Dominik Schramm <1610640051@fh-burgenland.at>
2020-12-12 17:55:53 +0100

    Added Spring Boot

---
+++ src/main/java/at/fhcampuswien/cyberpirates/WillBot/WillBotApplication.java
@@ -0,0 +1,13 @@
+package at.fhcampuswien.cyberpirates.WillBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhcampuswien/cyberpirates/WillBot/WillBotApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhcampuswien.cyberpirates.WillBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
