762cbeb8b83b6e23afd739fa137cd3f442bd3eb9
Martin Siska <mato.siska@gmail.com>
2020-12-19 18:09:57 +0100

    Dataclasses added

---
+++ src/main/java/at/fhcampuswien/cyberpirates/WillBot/objects/DoubleRange.java
@@ -0,0 +1,9 @@
+package at.fhcampuswien.cyberpirates.WillBot.objects;
+
+import lombok.Data;
+
+@Data
+public class DoubleRange {
+    private double from;
+    private double to;
+}
---
+++ src/main/java/at/fhcampuswien/cyberpirates/WillBot/objects/IntegerRange.java
@@ -0,0 +1,9 @@
+package at.fhcampuswien.cyberpirates.WillBot.objects;
+
+import lombok.Data;
+
+@Data
+public class IntegerRange {
+    private int from;
+    private int to;
+}
