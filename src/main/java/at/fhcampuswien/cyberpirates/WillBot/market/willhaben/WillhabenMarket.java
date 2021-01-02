492239fcb449f3030687e647bbd3c034de6b4978
Martin Siska <mato.siska@gmail.com>
2021-01-02 20:39:27 +0100

    added looping through Willhaben with map

--- src/main/java/at/fhcampuswien/cyberpirates/WillBot/market/willhaben/WillhabenMarket.java
+++ src/main/java/at/fhcampuswien/cyberpirates/WillBot/market/willhaben/WillhabenMarket.java
@@ -15,56 +15,8 @@ import java.util.List;
 public class WillhabenMarket implements IMarket {
 
     public static void main(String[] args) throws Exception {
-
-        List<String> allLinkText = new ArrayList<>();
-
-        //WebDriver driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); //keine Oberfläche nur der Code läuft im Hintergrund
-        WebDriver driver = new ChromeDriver(options);
-
-        driver.get("https://www.willhaben.at/iad/immobilien/mietwohnungen/mietwohnung-angebote?sfId=" +
-                "3800c179-a691-4579-b182-97f95d41d407&isNavigation=true&page=1&rows=25"); //connection to the website
-        driver.manage().window().maximize();
-        driver.findElement(By.xpath("//*[@class='didomi-components-button didomi-button didomi-dismiss-button didomi-" +
-                "components-button--color didomi-button-highlight highlight-button']")).click(); //close the cookies pop-up
-
-        List<WebElement> searchPageResults;
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        Thread.sleep(500);
-        js.executeScript("window.scrollBy(0,1000)","");
-        js.executeScript("window.scrollBy(1000,1600)","");
-        js.executeScript("window.scrollBy(1600,1800)","");
-        js.executeScript("window.scrollBy(1800,2100)","");
-
-        for (int i = 1; i <= 5; i++) { //iterate over pages
-
-            if (i > 1) {
-                driver.findElement(By.xpath("//a[@class='Button__ButtonContainer-sc-3uaafx-0 RVfyr Pagination__PaginationButton-zvrf30-1 Pagination__" +
-                        "PaginationItemButton-zvrf30-2 iYAkvt dPQbTP' and text()='"+i+"']")).click();
-                Thread.sleep(500);
-                js.executeScript("window.scrollBy(0,1000)","");
-                js.executeScript("window.scrollBy(1000,1600)","");
-                js.executeScript("window.scrollBy(1600,1800)","");
-                js.executeScript("window.scrollBy(1800,2100)","");
-            }
-
-            searchPageResults = driver.findElements(By.xpath("//div[@class='Box-wfmb7k-0 ResultListAdRowLayout___StyledBox-sc" +
-                  "-1rmys2w-0 iRWwJP gnPcbh']/div[@class='Box-wfmb7k-0 hkyQgZ']/a"));
-
-            for (WebElement link: searchPageResults) { //getting href link for each real estate
-                allLinkText.add(link.getAttribute("href"));
-            }
-
-            /*for (WebElement link: searchPageResults) {
-                allLinkText.add(link.getText());
-            }*/
-        }
-        for (String eachLinkText : allLinkText){ //print out names of the immo
-            System.out.println(eachLinkText);
-        }
-        driver.close();
+        WillhabenMarket market = new WillhabenMarket();
+        market.getResult(null);
     }
 
     @Override
