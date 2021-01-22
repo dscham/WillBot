0e1fd8990bd7f6c37aa8618a6050dda722f24eaf
AnnaRedl <anna.redl@stud.fh-campuswien.ac.at>
2021-01-22 09:46:28 +0100

    commenting the code disabling the searchbutton adding a loader

--- src/main/resources/static/xhr.js
+++ src/main/resources/static/xhr.js
@@ -1,10 +1,20 @@
+//creating variables for the buttons and the loader
+
 const searchBtn = document.getElementById('search');
 const resetBtn = document.getElementById('reset');
-//const input = document.getElementById('input');
+
+const loader = document.getElementById('loader');
 
 
+
+//Method for Searchbutton
 const getData = () => {
+    //making the loader visible and disabeling the searchubutton
+    loader.style.display = "block";
+    searchBtn.disabled = true;
 
+
+    //making variables from the input fields
     var plz = document.getElementById('plz').value;
     var pricefrom = document.getElementById('pfrom').value;
     var priceto = document.getElementById('pto').value;
@@ -14,22 +24,6 @@ const getData = () => {
     var roomCount = document.getElementById('rcount').value;
 
 
-    /*
-    var provider = document.querySelectorAll('input[name="provider"]');
-    var willhaben = true;
-    var immowelt = false;
-    if (provider[0].checked === true) {
-            willhaben = true;
-    }else{
-        willhaben = false;
-    }
-    if (provider[1].checked === true) {
-        immowelt = true;
-    }else{
-        immowelt = false;
-    }
-    */
-
 
     console.log('Searchbutton clicked');
     const xhr = new XMLHttpRequest();
@@ -37,16 +31,19 @@ const getData = () => {
     var table = document.getElementById('table')
 
 
+    //checking if all fields have an input
     if (plz === '' || pricefrom === '' || priceto === '' || areafrom === '' || areato === '' || roomCount === '') {
         alert("Bitte füllen Sie alle benötigten Felder aus ");
     } else {
 
+        //request to server and getting json data back
         const url = 'http://localhost:8080/willbot/api/request?postCode=' + plz + '&priceTo=' + priceto + '&priceFrom=' + pricefrom;
         console.log('searching......please be patient with me');
         xhr.open('GET', url);
         xhr.responseType = 'json';
 
         xhr.onload = () => {
+            //checking if there is data
             var data = xhr.response;
             if (data === '') {
                 console.log('Invalid Input');
@@ -55,6 +52,7 @@ const getData = () => {
                 var output = [];
 
 
+                //making an array out of the json data
                 for (var i = 0; i < data.length; i++) {
                     output.push(data[i].href);
                     output.push(data[i].title);
@@ -64,7 +62,6 @@ const getData = () => {
                     output.push(data[i].livingArea);
                     output.push(data[i].roomCount);
                     output.push(data[i].provider);
-
                 }
 
 
@@ -74,7 +71,7 @@ const getData = () => {
                 var i = 0;
                 var r = 0;
 
-
+                //making a 2dimensional array out of our first array
                 while (i < output.length) {
                     result[r] = new Array(8);
                     for (j = 0; j < 8; j++) {
@@ -86,6 +83,7 @@ const getData = () => {
 
                 var c = 0;
 
+                //Cheking the output
                 if (result.length === 0) {
                     alert('Keine passende Ergebnisse gefunden');
                 } else {
@@ -103,6 +101,7 @@ const getData = () => {
                                     if(result[i][columnCount - 3] > areato){
                                         console.log('areato filtered');
                                     }else {
+                                        //creating the rows that get filled into the table
                                         var row = table.insertRow(-1);
                                         c++;
                                         for (var j = 0; j < columnCount; j++) {
@@ -117,19 +116,48 @@ const getData = () => {
                 }if(c === 0){
                     alert('Keine passenden Ergebnisse gefunden');
                 }else{
-                    searchBtn.style.visibility = 'hidden';
+                    //enanbeling the search button and hiding the loader
+                    searchBtn.disabled = false;
+                    loader.style.display = "none";
                 }
             }
         };
+
         xhr.send();
     }
 };
 
 
+//Method for reset button
+//Resets the whole Page
 const resetData = () => {
     console.log('Resetbutton clicked');
     window.location.reload();
 };
 
+// creating event listeners for the buttons
 searchBtn.addEventListener('click', getData);
 resetBtn.addEventListener('click', resetData);
+
+
+
+
+
+
+
+
+/*
+var provider = document.querySelectorAll('input[name="provider"]');
+var willhaben = true;
+var immowelt = false;
+if (provider[0].checked === true) {
+        willhaben = true;
+}else{
+    willhaben = false;
+}
+if (provider[1].checked === true) {
+    immowelt = true;
+}else{
+    immowelt = false;
+}
+*/
